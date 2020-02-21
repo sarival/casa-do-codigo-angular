466fd5310f15018a24bc2d23f2135c9de087039a
Leonardo Rodrigues de Souza <sarival@sarival.com.br>
2020-02-21 20:02:02 -0300

    tornando as mensagens de validação dinamicas

--- src/app/admin/autor/cadastro-autor/cadastro-autor.component.ts
+++ src/app/admin/autor/cadastro-autor/cadastro-autor.component.ts
@@ -1,20 +1,46 @@
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, AfterViewInit, ViewChildren, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControlName } from '@angular/forms';
 import { Autor } from 'src/app/models/autor.model';
 import { DataService } from 'src/app/services/data.service';
+import { ValidationMessages, GenericValidator, DisplayMessage } from '../../generic-form-validation';
+import { Observable, fromEvent, merge } from 'rxjs';
 
 @Component({
   selector: 'app-cadastro-autor',
   templateUrl: './cadastro-autor.component.html'
 })
-export class CadastroAutorComponent implements OnInit {
+export class CadastroAutorComponent implements OnInit, AfterViewInit {
+
+  //add
+  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
   
   public cadastroForm: FormGroup;
   public autor: Autor;
 
+  //add variaveis
+  public validationMessages: ValidationMessages;
+  public genericValidator: GenericValidator;
+  public displayMessage: DisplayMessage = {};
+
   constructor(private fb: FormBuilder, private service: DataService, private router: Router) {
     
+    //add nmensagens para serem exibidas
+    this.validationMessages = {
+      nome: {
+        required: 'O Nome é requerido',
+      },
+      email: {
+        required: 'Informe o e-mail',
+        email: 'Email inválido'
+      },
+      descricao: {
+        required: 'A Descrição é requerida',
+        maxlength: 'O Nome precisa ter no máximo 400 caracteres'
+      },
+    };
+
+    this.genericValidator = new GenericValidator(this.validationMessages);
   }
   
   ngOnInit(){
@@ -25,9 +51,21 @@ export class CadastroAutorComponent implements OnInit {
     });
   }
 
+  //add - apos a tela ser renderizada
+  ngAfterViewInit(): void {
+    let controlBlurs: Observable<any>[] = this.formInputElements
+    .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+
+    merge(...controlBlurs).subscribe(() => {
+      this.displayMessage = this.genericValidator.processarMensagens(this.cadastroForm);
+    });
+  }
+
   adicionarAutor(){
     if(this.cadastroForm.valid){
       this.autor = Object.assign({}, this.autor, this.cadastroForm.value);
+
+      alert("valido");
       
       this.service.cadastrarAutor(this.autor).subscribe( res => {this.router.navigateByUrl("/")} );
     }
